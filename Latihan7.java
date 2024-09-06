03d119c608bfcab1b4345973061aa714181fe121
Yulia Nadzifah <ilhansalihilsa@gmail.com>
2024-09-06 09:59:42 +0700

    commitPraktikum01

---
+++ Latihan7.java
@@ -0,0 +1,12 @@
+/**
+ * Latihan7
+ */
+public class Latihan7 {
+    public static void main(String[] args) {
+        System.out.print("Nama Saya Ilhan Salih Ilmansyah");
+        //System.out.print("\nsaya adalah mahasiswa baru Jurusan Teknologi Informasi-POLINEMA");
+        
+    }
+
+    
+}
---
+++ Tugas1.java
@@ -0,0 +1,11 @@
+public class Tugas1 {
+    public static void main(String[] args) {
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama    : Ilhan Salih Ilmansyah");
+        System.out.println("Absen   : 7");
+        System.out.println("Kelas   : 1D");
+        System.out.println("Prodi   : D4 Sistem Informasi Bisnis");
+        System.out.println("Jurusan : Teknologi Informasi");
+    }
+    
+}
